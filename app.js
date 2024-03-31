55c218a4b23b3e5a10b889cf6243c26ff7099c25
musman-js <usman2037966@gmail.com>
2024-03-31 16:00:43 +0500

    Form designing is pending

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-
-const name = document.getElementById("name");
+const nameInput = document.getElementById("name");
 const fatherName = document.getElementById("fatherName");
 const formName = document.getElementById("formName");
 const button = document.getElementById('generate');
 const formFatherName = document.getElementById('formFatherName');
+const formMain = document.getElementById("formMain");
+const information = document.getElementById("information");
+
+//Disable the form when the informtaion is not given
+formMain.style.display = "none"
 
 
 
-function generate() {
-    formName.innerHTML = name.value
+function generate() { 
+    //Sending Input to Form
+    formName.innerHTML = nameInput.value
     formFatherName.innerHTML = fatherName.value
+    information.style.display =  "none";
+     
+    //Display form when the generate button is clicked
+    formMain.style.display = "block";   
+   
 }
 
 
