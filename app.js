1544c5b1fb63addb3acb322e45aac943939e4cd5
musman-js <usman2037966@gmail.com>
2024-04-01 18:45:27 +0500

    Sending input to Form is pending

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 const nameInput = document.getElementById("name");
 const fatherName = document.getElementById("fatherName");
-const formName = document.getElementById("formName");
-const button = document.getElementById('generate');
-const formFatherName = document.getElementById('formFatherName');
-const formMain = document.getElementById("formMain");
-const information = document.getElementById("information");
+const dateOfBirth = document.getElementById("date");
+const email = document.getElementById("email");
+const education = document.getElementBy;
+const CNIC = document.getElementById("CNIC");
+const contact = document.getElementById("contact");
+const address = document.getElementById("address");
+const field = document.getElementById("button");
+const country = document.getElementById("button");
+
+
+
+
+
+// const formName = document.getElementById("formName");
+// const button = document.getElementById('generate');
+// const formFatherName = document.getElementById('formFather');
+// const formMain = document.getElementById("formMain");
+// const information = document.getElementById("information");
+// const backButton = document.getElementById("button");
 
 //Disable the form when the informtaion is not given
-information.style.display = "none"
+formMain.style.display = "none"
 
-
-
-function generate() { 
+function generate() {
     //Sending Input to Form
     formName.innerHTML = nameInput.value
     formFatherName.innerHTML = fatherName.value
-    information.style.display =  "none";
-     
+    information.style.display = "none";
+
     //Display form when the generate button is clicked
-    formMain.style.display = "block";   
-   
+    formMain.style.display = "block";
+
 }
 
+
+backButton.addEventListener(onclick, () => {
+    formMain.style.display = "none"
+    information.style.display = "flex"
+}
+)
 
